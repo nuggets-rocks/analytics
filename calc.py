c80e7d9a5577874a552ccee931b09dcc80e67225
chivagarg <chivagarg@gmail.com>
2019-03-23 12:23:46 -0700

    initial commit

---
+++ calc.py
@@ -0,0 +1,33 @@
+import json
+from pprint import pprint
+pprint("hello world")
+with open('Nugget.json') as f:
+	data = json.load(f)
+pprint(len(data['results']))
+
+total = 0;
+tags = 0;
+#owner = 0;
+ownerToNug = {}
+for nugget in data['results']:
+	if nugget.get('tags') is not None:
+		tags+=1
+	if nugget.get('owner') is not None:
+		owner = nugget.get('owner')
+		objectId = owner.get('objectId')
+		if ownerToNug.get(objectId) is not None:
+			#pprint("Found!!!!")
+			# No owner entry existed
+			ownerToNug[objectId] = ownerToNug[objectId] + 1
+		else:
+			ownerToNug[objectId] =  1
+#		owner+=1
+	total+=1
+
+for key in ownerToNug:
+	pprint(key + "," + str(ownerToNug[key]))
+
+
+pprint("total is " + str(total))
+pprint("tags is " + str(tags))
+pprint("% of nuggets with tags is " + str((tags*100)/total))	
